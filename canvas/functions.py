7d383de154047c4f696cf63ba3ad3ac9378b626e
Sébastien <sebastien.works@outlook.com>
2024-10-11 21:37:09 +0200

    Update functions.py

--- canvas/functions.py
+++ canvas/functions.py
@@ -1,1 +1,11 @@
+import kandinsky
 
+# Initialization error
+class InitError(Exception):
+  """Raised exception for initialization issues : the canvas must be initialized before adding drawings."""
+  pass
+
+# Missing argument error
+class MissingArgumentError(Exception):
+  """Raised exception for missing arguments or invalid type input."""
+  pass
