99bbf847ef2a827116c692a6ab02ecd36c6d4b50
Sébastien <sebastien.works@outlook.com>
2024-10-12 14:57:51 +0200

    Create main.py

---
+++ examples/Circle/main.py
@@ -0,0 +1,6 @@
+# Draw a circle with center at (160, 111), radius 50, and red color
+circle = Circle(x=160, y=111, radius=50, color="red")
+circle.draw()  # Draw the circle on the canvas
+
+# Destroy the circle
+circle.destroy()
