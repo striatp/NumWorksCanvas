dec80aefd8107eff5d17b614521c3b68c8ea86f1
Sébastien <sebastien.works@outlook.com>
2024-10-12 14:47:45 +0200

    Update canvas.py

--- canvas/canvas.py
+++ canvas/canvas.py
@@ -1,7 +1,17 @@
 from .functions import InitError
+from .functions import Rectangle
+from .functions import Triangle
 from .functions import Canvas
 from .functions import Circle
-from .functions import Rectangle
 from .functions import Text
-from .functions import Triangle
 from .functions import Line
+
+__all__ = [
+  InitError,
+  Rectangle,
+  Triangle,
+  Canvas,
+  Circle,
+  Text,
+  Line
+]
