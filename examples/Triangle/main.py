735b4d023817886651a3a3a00022e0f0d70a74f3
Sébastien <sebastien.works@outlook.com>
2024-10-12 14:58:55 +0200

    Create main.py

---
+++ examples/Triangle/main.py
@@ -0,0 +1,6 @@
+# Draw a triangle with vertices at (30, 30), (80, 30), and (55, 80)
+triangle = Triangle(x1=30, y1=30, x2=80, y2=30, x3=55, y3=80)
+triangle.draw()  # Draw the triangle on the canvas
+
+# Destroy the triangle
+triangle.destroy()
