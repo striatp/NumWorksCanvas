1dead9212f441ac78a4a6742c21f56e1a1f3e381
Sébastien <sebastien.works@outlook.com>
2024-10-12 16:14:53 +0200

    Update canvas.py

--- canvas/canvas.py
+++ canvas/canvas.py
@@ -1,10 +1,10 @@
-from .functions import InitError
-from .functions import Rectangle
-from .functions import Triangle
-from .functions import Canvas
-from .functions import Circle
-from .functions import Text
-from .functions import Line
+from functions import InitError
+from functions import Rectangle
+from functions import Triangle
+from functions import Canvas
+from functions import Circle
+from functions import Text
+from functions import Line
 
 # Public API : InitError is not raisable by users
 __all__ = [
